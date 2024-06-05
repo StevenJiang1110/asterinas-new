2a1c54fe08c2c9f6af2296ba61a73aedb5d35d29
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2024-06-05 06:32:04 +0000

    Add CI to update API documentations

---
+++ tools/github_workflows/generate_redirect_index_html.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: MPL-2.0
+
+# This script is used to generated redirecting index.html for API documentation website.
+
+set -e
+
+print_help() {
+    echo "Usage: $0 url [output]"
+    echo "url:      The URL to which index.html will be directed."
+    echo "output:   The path to the generated HTML file. Default is index.html in the current directory."
+}
+
+validate_parameter() {
+    if [ "$#" -eq 0 ]; then
+    echo "Error: At least one parameter is required."
+    print_help
+    exit 1
+    fi
+
+    if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+        print_help
+        exit 0
+    fi
+}
+
+validate_parameter
+URL=$1
+OUTPUT=${2:-index.html}
+
+template="
+<!DOCTYPE html>\n
+<html>\n
+<head>\n
+  <meta http-equiv=\"refresh\" content=\"0; URL=${URL}\">\n
+</head>\n
+<body>\n
+  <p>Redirecting to a new page...</p>\n
+  <script>\n
+    // If the browser doesn't support automatic redirection, display a link for manual redirection\n
+    window.location.href = \"${URL}\";\n
+  </script>\n
+</body>\n
+</html>\n
+"
+
+echo -e ${template} > ${INDEX_HTML_PATH}
+
+
