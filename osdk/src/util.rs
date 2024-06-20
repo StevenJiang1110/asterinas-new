0b44b595f8e392fde83bcbe102354f9a48d2d8a7
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2024-06-20 06:16:04 +0000

    Rename aster_main as ostd::main

--- osdk/src/util.rs
+++ osdk/src/util.rs
@@ -89,7 +89,7 @@ pub struct CrateInfo {
 /// If there are multiple kernel crates or no kernel crates in the workspace,
 /// this function will exit with an error.
 ///
-/// A crate is considered a kernel crate if it utilizes the `aster_main` macro.
+/// A crate is considered a kernel crate if it utilizes the `ostd::main` macro.
 fn get_default_member(metadata: &serde_json::Value) -> &str {
     let default_members = metadata
         .get("workspace_default_members")
@@ -125,7 +125,7 @@ fn get_default_member(metadata: &serde_json::Value) -> &str {
                     syn::parse_file(&content).unwrap()
                 };
 
-                contains_aster_main_macro(&file)
+                contains_ostd_main_macro(&file)
             })
             .collect()
     };
@@ -143,7 +143,7 @@ fn get_default_member(metadata: &serde_json::Value) -> &str {
     packages[0].get("id").unwrap().as_str().unwrap()
 }
 
-fn contains_aster_main_macro(file: &syn::File) -> bool {
+fn contains_ostd_main_macro(file: &syn::File) -> bool {
     for item in &file.items {
         let syn::Item::Fn(item_fn) = item else {
             continue;
@@ -151,7 +151,7 @@ fn contains_aster_main_macro(file: &syn::File) -> bool {
 
         for attr in &item_fn.attrs {
             let attr = format!("{}", attr.to_token_stream());
-            if attr.as_str() == "# [aster_main]" {
+            if attr.as_str() == "# [ostd :: main]" || attr.as_str() == "#[main]" {
                 return true;
             }
         }
